923f52298b49260c7a2c72e3da84fe4a440a77d0
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-01-31 18:02:09 +0530

    Add Pro6

---
+++ Assignment_2/src/assi_2/Pro6.java
@@ -0,0 +1,21 @@
+package assi_2;
+
+public class Pro6 
+{
+
+	 final int speed=40;
+	
+	void change(int a)
+	{
+		speed=a;//can't assign a value to final variable speed
+		System.out.println("New Speed:"+speed);
+	}
+	
+	public static void main(String...args)
+	{
+			Pro6 p =new Pro6();
+			p.change(70);//can't assign a value to final variable speed
+			
+			
+	}
+}
