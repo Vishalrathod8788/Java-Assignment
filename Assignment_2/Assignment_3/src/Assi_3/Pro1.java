443e8e6f7e713dff240e30169b3ee75bc54f9a3b
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-01 20:31:46 +0530

    Add...

---
+++ Assignment_2/Assignment_3/src/Assi_3/Pro1.java
@@ -0,0 +1,17 @@
+
+
+package Assi_3;
+
+import java.util.Date;
+
+public class Pro1 
+{
+	public static void main(String[] args) 
+	{
+		Date dt = new Date();
+		System.out.println(dt);
+		System.out.println( "Date :" +  dt.getDate());
+		
+		System.out.println("get time:" + dt.getTime());
+	}
+}
---
+++ Assignment_3/src/assi_3/Pro1.java
@@ -0,0 +1,15 @@
+package assi_3;
+
+import java.util.Date;
+
+public class Pro1
+{
+	public static void main(String[] args) 
+	{
+		Date dt = new Date();
+		System.out.println(dt);
+		System.out.println( "Date :" +  dt.getDate());
+		
+		System.out.println("Time:" + dt.getTime());
+	}
+}
