b8bbf983fe8a5f78d08758c4671b205833f43bb2
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-01 20:40:10 +0530

    Add...

--- Assignment_3/src/assi_3/Pro1.java
+++ Assignment_3/src/assi_3/Pro1.java
@@ -1,12 +1,12 @@
 //Author : Vishal Rathod
-//Date 01/03/2023
-//Time 8:33 PM
+//Date : 01/03/2023
+//Time : 08:39 PM
 
 package assi_3;
 
 import java.util.Date;
 
-public class Pro1
+public class Pro1 
 {
 	public static void main(String[] args) 
 	{
@@ -14,6 +14,6 @@ public class Pro1
 		System.out.println(dt);
 		System.out.println( "Date :" +  dt.getDate());
 		
-		System.out.println("Time:" + dt.getTime());
+		System.out.println("get time:" + dt.getTime());
 	}
 }
