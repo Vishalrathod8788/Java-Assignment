3e8bbd1005fddc5693e93a40dabc9e953429fbe5
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-01 20:54:14 +0530

    Add...

---
+++ Assignment_3/src/assi_3/Pro3.java
@@ -0,0 +1,20 @@
+//Author : Vishal Rathod
+//Date : 01/03/2023
+//Time : 08:40 PM
+
+package assi_3;
+
+import java.util.Random;
+
+public class Pro3
+{
+	public static void main(String[] args) 
+	{
+		Random random = new Random();
+		for(int n=0;n<=n;n++)
+		{
+			System.out.println(random.nextInt());
+		}
+
+	}
+}
