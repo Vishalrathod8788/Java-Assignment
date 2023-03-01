af9eb7cfe8be47ecbd785d529384a84255855b47
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-01 20:58:31 +0530

    Add...

--- Assignment_3/src/assi_3/Pro3.java
+++ Assignment_3/src/assi_3/Pro3.java
@@ -5,12 +5,18 @@
 package assi_3;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Pro3
 {
 	public static void main(String[] args) 
 	{
+		int a;
+		
+		
+		
 		Random random = new Random();
+		
 		for(int n=0;n<=n;n++)
 		{
 			System.out.println(random.nextInt());
