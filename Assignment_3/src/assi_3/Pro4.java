6e52a69cbe105f3ff1781f904898e5ef60dbbff3
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-02 23:52:25 +0530

    Ad

---
+++ Assignment_3/src/assi_3/Pro4.java
@@ -0,0 +1,21 @@
+package assi_3;
+
+import java.util.Vector;
+
+public class Pro4 {
+
+	
+		public static void main(String[] args)
+		{
+			Vector v1 = new Vector();
+			
+			String str = "Visha ";
+			v1.addElement(str);
+			v1.insertElementAt("Hello Friends", 2);
+			v1.size();
+			v1.removeElement("Hello Friends");
+			v1.clear();
+			
+		}
+	
+}
