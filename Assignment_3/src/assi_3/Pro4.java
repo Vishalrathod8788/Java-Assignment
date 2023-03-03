5b66a7536662e16bb2415f0069100ed007373b34
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-03 21:38:06 +0530

    Add...

--- Assignment_3/src/assi_3/Pro4.java
+++ Assignment_3/src/assi_3/Pro4.java
@@ -2,17 +2,49 @@ package assi_3;
 
 import java.util.Vector;
 
+class VectorDemo 
+{
+	private int no;
+	private String name;
+
+	int getNo(int no)
+	{
+		return no;
+	}
+	public VectorDemo(int no, String name) {
+		super();
+		this.no = no;
+		this.name = name;
+	}
+
+
+	@Override
+	public String toString() {
+		return "No = " + no + " Name = " + name ;
+	}
+
+}
 public class Pro4 {
 
 	
-		public static void main(String[] args)
+		public static void main(String...args)
 		{
-			System.out.println("Hii...");
 			Vector v1 = new Vector();
 			
-			v1.addElement("VGR");
+			VectorDemo v = new VectorDemo(1, "Vishal");
+			VectorDemo v0 = new VectorDemo(2, "VGR"); 
 			
-			System.out.println(v1);
-		}
-	
+			v1.addElement("VGR");
+			System.out.println("Vecor Size " + v1.size());
+			v1.addElement(v);
+			v1.addElement(v0);
+			v1.insertElementAt("Vishal", 1);
+			v1.addElement(v.getNo(1));
+			v1.clear();
+			
+			for (int i = 0; i < v1.size(); i++)
+			{
+				System.out.println(i +" : "+v1.get(i));
+			}
+		}	
 }
