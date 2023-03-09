29cdec9ea75c79cde86a2410a885e97a57fbb97a
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-09 17:57:35 +0530

    Add...

---
+++ Assignment_3/src/assi_3/Pro6.java
@@ -0,0 +1,17 @@
+package assi_3;
+
+import java.util.StringTokenizer;
+
+public class Pro6 {
+
+	public static void main(String[] args) 
+	{
+		String str = "hello;i:study;in:Bhavans's;shree:H.J.;Doshi;IT:Institute";
+		StringTokenizer st = new StringTokenizer(str, ";:'.");
+		while(st.hasMoreTokens())
+		{
+			System.out.println(st.nextToken());
+		}
+	}
+
+}
