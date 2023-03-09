5fc5621b6eb38327c2e2ec6102864f01006c6f39
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-09 20:04:04 +0530

    Add...

--- Assignment_3/src/assi_3/Pro9.java
+++ Assignment_3/src/assi_3/Pro9.java
@@ -4,7 +4,11 @@ public class Pro9 {
 
 	public static void main(String[] args) 
 	{
+		System.out.println("This is Main Thread Demo...!");
 		
+		Thread t = Thread.currentThread();
+		
+		System.out.println("Name : " + t.getName());
 	}
 
 }
