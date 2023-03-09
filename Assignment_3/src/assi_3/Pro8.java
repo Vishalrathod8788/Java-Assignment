158d40f098c3f1e632696a6a4849e97ac647df42
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-09 19:34:41 +0530

    Add...

---
+++ Assignment_3/src/assi_3/Pro8.java
@@ -0,0 +1,19 @@
+package assi_3;
+
+import java.util.Scanner;
+
+public class Pro8 {
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int age;
+		
+		System.out.println("Program Start...!");
+		
+		
+		
+		System.out.println("Program End...!");
+	}
+
+}
