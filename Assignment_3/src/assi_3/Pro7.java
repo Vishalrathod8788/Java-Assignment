44704911091ed625e43d24820d555dbfd228e661
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-09 19:21:04 +0530

    Add...

--- Assignment_3/src/assi_3/Pro7.java
+++ Assignment_3/src/assi_3/Pro7.java
@@ -4,7 +4,30 @@ public class Pro7 {
 
 	public static void main(String[] args) 
 	{
+		System.out.println("Program Start....");
+			
+		String name[] = {""};
+		String address[] = {"Add1", "Add2", "Add3"};
+		try {
+			
+			int x = 10/0;									// This is General Error
+			System.out.println(name[1]);					// This is NullPointerException Error			
+			System.out.println(address[3]);					// This is ArrayIndexOutOfBoundsException Error
+		}
+		catch (NullPointerException arr) 
+		{
+			System.out.println("Null Pointer Exception");
+		}
+		catch (ArrayIndexOutOfBoundsException arr) 
+		{
+			System.out.println("Array Index Out Of Bound");
+		}
+		catch (Exception e) 
+		{
+			System.out.println("General Error...!");
+		}
 		
+		System.out.println("Program End....");
 	}
 
 }
