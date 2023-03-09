4b8730f7e7c6ffcad5ebb9a99a45e1f78268b610
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-09 17:15:44 +0530

    Add...

--- Assignment_3/src/assi_3/Pro5.java
+++ Assignment_3/src/assi_3/Pro5.java
@@ -6,7 +6,19 @@ public class Pro5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Hashtable ht = new Hashtable();
+		demo d1, d2;
+		d1 = new demo(1, "Vishal");
+		ht.put(d1.getId(), d1);
+		d1 = new demo(2, "VGR");
+		ht.put(d1.getId(), d1);
+		d2 = (demo) ht.get(2);
+		System.out.println(d2.getName());
+		System.out.println(d2.getId());
+		System.out.print("\n");
+		d2 = (demo) ht.get(1);
+		System.out.println(d2.getId());
+		System.out.println(d2.getName());
 	}
 
 }
