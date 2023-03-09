7cd5b241975b342403804cb233d08e3096d8bb0f
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-09 20:19:45 +0530

    Add...

--- Assignment_3/src/assi_3/Pro9.java
+++ Assignment_3/src/assi_3/Pro9.java
@@ -6,9 +6,22 @@ public class Pro9 {
 	{
 		System.out.println("This is Main Thread Demo...!");
 		
-		Thread t = Thread.currentThread();
+		Thread t = Thread.currentThread();							// Current Thread 
 		
 		System.out.println("Name : " + t.getName());
+		
+		t.setName("MAIN THREAD...!");								// Thread Name Changes
+		
+		System.out.println("Change Name : " + t.getName());
+		System.out.println("Thread Priority Name : " + t.getPriority());
+		System.out.println("Thread Id : "+ t.getId());
+		System.out.println("Thread Alive : "+t.isAlive());
+		System.out.println("Thread Class Name : "+t.getClass());
+		System.out.println("Thread MAX Priority : "+t.MAX_PRIORITY);
+		System.out.println("Thread MIN Priority : "+t.MIN_PRIORITY);
+		System.out.println("Thread Normal Priority : "+t.NORM_PRIORITY);
+		System.out.println("Thread Active Count : "+t.activeCount());
+		System.out.println("Main Thread : "+ t);
 	}
 
 }
