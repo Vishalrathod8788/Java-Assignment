6b21258cb0f43e00240b528035c3c09fcc7588b0
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-10 23:01:25 +0530

    Add...

--- Assignment_3/src/assi_3/Pro12.java
+++ Assignment_3/src/assi_3/Pro12.java
@@ -2,9 +2,26 @@ package assi_3;
 
 public class Pro12 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		
 	}
 
+}
+class First
+{
+	synchronized void call(String str)
+	{
+		System.out.println("["+str);
+		try
+		{
+			Thread.sleep(1000);
+			
+		} 
+		catch(InterruptedException e)
+		{
+			System.out.println("Interrupted");
+		}
+		System.out.println("]");
+	}
 }
