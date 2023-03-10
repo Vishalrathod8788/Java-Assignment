9fcf809b28cb8781766151c2ae81c4c47130438d
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-10 22:51:59 +0530

    Add...

--- Assignment_3/src/assi_3/Pro11.java
+++ Assignment_3/src/assi_3/Pro11.java
@@ -3,15 +3,26 @@ package assi_3;
 public class Pro11 implements Runnable 
 {
 
+	@Override
+	public void run() 
+	{
+		while(true)
+		{
+			try 
+			{
+				Thread.sleep(1000);
+				System.out.println("Upload...");
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
 	public static void main(String[] args) 
 	{
 
 	}
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 }
