8ad6dac55791e281d44b57f7a2596b82dd4306c6
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-10 22:45:20 +0530

    Add...

--- Assignment_3/src/assi_3/Pro10.java
+++ Assignment_3/src/assi_3/Pro10.java
@@ -1,14 +1,26 @@
 package assi_3;
 
-public class Pro10 
+public class Pro10 extends Thread
 {
 	public void run()
 	{
-		
+		while(true)
+		{
+			try 
+			{
+				Thread.sleep(1000);
+				System.out.println("Download...!");
+			}
+			catch (Exception e) 
+			{
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public static void main(String[] args) 
 	{
-		
+		Pro10 p = new  Pro10();
+		p.start();
 	}
 }
